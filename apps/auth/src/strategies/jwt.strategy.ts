7bcb1bc2485c81a8c84e9e50b0b187c9f01b48b6
ahurein <ahureinebenezer@gmail.com>
2023-11-06 18:23:39 +0000

    add tcp communicatioin between services

--- apps/auth/src/strategies/jwt.strategy.ts
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -1,8 +1,6 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { UsersService } from "../users/users.service";
-import { Request } from "express";
-import { TokenPayload } from "../interfaces/token-payload.interface";
 import { ConfigService } from "@nestjs/config";
 import { Injectable } from "@nestjs/common";
 
@@ -14,15 +12,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => request?.cookies?.Authentication
+                (request: any) => request?.cookies?.Authentication || request?.Authentication
             ]),
             secretOrKey: configService.get<String>('JWT_SECRET'),
         })
     }
 
     async validate({userId}){
-        console.log(userId)
-        
         return this.userService.getUser({_id: userId})
     }
 }
