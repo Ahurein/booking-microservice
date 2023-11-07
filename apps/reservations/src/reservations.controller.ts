8dbe41706241dd33055ecef829547984ee506fbd
ahurein <ahureinebenezer@gmail.com>
2023-11-07 09:25:00 +0000

    complete auth and reservations

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -11,7 +11,7 @@ import {
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
-import { JwtAuthGuard } from '@app/common';
+import { CurrentUser, JwtAuthGuard, UserDto } from '@app/common';
 
 @Controller('reservations')
 export class ReservationsController {
@@ -19,8 +19,8 @@ export class ReservationsController {
 
   @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createReservationDto: CreateReservationDto) {
-    return this.reservationsService.create(createReservationDto);
+  create(@Body() createReservationDto: CreateReservationDto, @CurrentUser() user: UserDto) {
+     return this.reservationsService.create(createReservationDto, user._id);
   }
 
   @Get()
--- libs/common/src/auth/jwt-auth-guard.ts
+++ libs/common/src/auth/jwt-auth-guard.ts
@@ -2,11 +2,10 @@ import { CanActivate, ExecutionContext, Inject } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { Observable, map, tap } from "rxjs";
 import { AUTH_SERVICE } from "../constants/services";
+import { UserDto } from "../dto";
 
 export class JwtAuthGuard implements CanActivate {
-    constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {
-
-    }
+    constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
 
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const jwt = context.switchToHttp().getRequest().cookies?.Authentication;
@@ -14,7 +13,7 @@ export class JwtAuthGuard implements CanActivate {
             return false
         }
 
-        return this.authClient.send("authenticate", {
+        return this.authClient.send<UserDto>("authenticate", {
             Authentication: jwt
         }).pipe(
             tap(res => {
